0de511c3a028bcfe75ca70cc4d047730ab852fdf
Jaifran Hernandez <jaifranjose@gmail.com>
2024-02-04 17:32:59 -0400

    Fix: Config and more

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,17 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      animation: {
+        orbit: 'spin 4s linear forwards infinite',
+        orbit2: 'spin 7s linear reverse infinite',
+        orbit3: 'spin 9s linear forwards infinite',
+        orbit4: 'spin 10s linear reverse infinite',
+      },
+      fontFamily: {
+        // 👇 Add CSS variables
+        sans: ["var(--font-gasoek-one)"],
+        mono: ["var(--font-roboto-mono)"],
+      },
     },
   },
   plugins: [],
